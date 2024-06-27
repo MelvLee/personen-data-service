4c1a75c88c6999e3de11af78bddec6d8b167fd5d
Melvin Lee <melvin.lee@iswish.nl>
2024-06-27 18:06:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BRP shared
+[![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
---
+++ scripts/specs-verify.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-zonder-dependency-integratie.json \
+                -f summary:./test-reports/cucumber-js/step-definitions/test-result-zonder-dependency-integratie-summary.txt \
+                -f summary \
+                features/docs \
+                --tags "not @integratie"
