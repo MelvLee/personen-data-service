990ddf069f23aad0dbfc465efedcc572605e4658
Melvin Lee <melvin.lee@iswish.nl>
2024-07-10 14:05:47 +0200

    chore: gezag mock en gezag proxy mock implementatie

---
+++ src/GezagProxyMock/Program.cs
@@ -0,0 +1,15 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile(Path.Combine("configuration", "ocelot.json"))
+                     .AddEnvironmentVariables();
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+app.UseOcelot().Wait();
+
+app.Run();
