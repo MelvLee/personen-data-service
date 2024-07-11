aec5f3b1f8c7b27c01d3300b529c3a2ba7c3fcde
Melvin Lee <melvin.lee@iswish.nl>
2024-07-11 07:35:50 +0200

    ci: separaat amd/arm container images ipv multi-platform container image

--- scripts/containers-build.sh
+++ scripts/containers-build.sh
@@ -3,9 +3,7 @@
 MODE=$1
 
 if [ "$MODE" = "ci" ]; then
-    docker buildx create --name container --driver docker-container --use
     docker compose -f .docker/docker-compose-ci.yml build
-    docker buildx rm --force container
 else
     docker compose -f src/docker-compose.yml build
 fi
