8b1c82aa8b5eee76ee1bdf476a90853d718346f7
Melvin Lee <melvin.lee@iswish.nl>
2024-07-16 16:07:08 +0200

    chore(ci): gebruik docker compose up om container images te builden en aan te maken in lokale registry

--- scripts/containers-build.sh
+++ scripts/containers-build.sh
@@ -3,7 +3,8 @@
 MODE=$1
 
 if [ "$MODE" = "ci" ]; then
-    docker compose -f .docker/docker-compose-ci.yml build
+    docker compose -f .docker/docker-compose-ci.yml up -d
+    docker compose -f .docker/docker-compose-ci.yml down
 else
     docker compose -f src/docker-compose.yml build
 fi
